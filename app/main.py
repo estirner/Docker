549768af68f6cc1b2db58a5a55ef1b43b65f290b
Sheroze <154746371+estirner@users.noreply.github.com>
2024-01-20 00:01:13 +0500

    pass fourth stage

--- app/main.py
+++ app/main.py
@@ -1,20 +1,25 @@
+import os
+import shutil
 import subprocess
 import sys
+import tempfile
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
-    
-    completed_process = subprocess.Popen(
-        [command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-    stdout, stderr = completed_process.communicate()
-    if stdout:
-        print(stdout.decode("utf-8"), end="")
-    if stderr:
-        print(stderr.decode("utf-8"), end="", file=sys.stderr)
 
-    sys.exit(completed_process.returncode)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        shutil.copy(command, tmpdir)
+        os.chroot(tmpdir)
+        completed_process = subprocess.Popen(
+            [command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        stdout, stderr = completed_process.communicate()
+        if stdout:
+            print(stdout.decode("utf-8"), end="")
+        if stderr:
+            print(stderr.decode("utf-8"), end="", file=sys.stderr)
+        sys.exit(completed_process.returncode)
 
 if __name__ == "__main__":
     main()
