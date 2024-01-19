d07b6b4ce2d889179ba82324f73b710ad123fe50
Sheroze <154746371+estirner@users.noreply.github.com>
2024-01-19 18:13:23 +0500

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
     stdout, _ = completed_process.communicate()
 
     if stdout:
-        print(stdout.decode("utf-8"), end="\n")
+        print(stdout.decode("utf-8"), end="")
 
 if __name__ == "__main__":
     main()
