d4afcc49ce57ce78b1ac8c4ecf295923d2ae120e
Sheroze <154746371+estirner@users.noreply.github.com>
2024-01-19 18:11:42 +0500

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -1,17 +1,15 @@
 import subprocess
 import sys
 
-
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
     
-    try:
-        output = subprocess.check_output([command, *args], stderr=subprocess.STDOUT)
-        print(output.decode("utf-8").rstrip())
-    except subprocess.CalledProcessError as e:
-        print(e.output.decode("utf-8").rstrip())
+    completed_process = subprocess.Popen([command, *args], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, _ = completed_process.communicate()
 
+    if stdout:
+        print(stdout.decode("utf-8").rstrip(), end="")
 
 if __name__ == "__main__":
     main()
