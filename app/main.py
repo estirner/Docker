72920fbd89fadb6d67448ddfc36f106e0456efd1
Sheroze <154746371+estirner@users.noreply.github.com>
2024-01-19 18:14:45 +0500

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -5,11 +5,14 @@ def main():
     command = sys.argv[3]
     args = sys.argv[4:]
     
-    completed_process = subprocess.Popen([command, *args], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    stdout, _ = completed_process.communicate()
-
+    completed_process = subprocess.Popen(
+        [command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    stdout, stderr = completed_process.communicate()
     if stdout:
         print(stdout.decode("utf-8"), end="")
+    if stderr:
+        print(stderr.decode("utf-8"), end="", file=sys.stderr)
 
 if __name__ == "__main__":
     main()
