871f6fc92985c8110893aad6ad6b416d098745a8
Sheroze <154746371+estirner@users.noreply.github.com>
2024-01-20 18:49:16 +0500

    pass fifth stage

--- app/main.py
+++ app/main.py
@@ -7,13 +7,20 @@ import tempfile
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
+
     directory_path = tempfile.mkdtemp()
     shutil.copy2(command, directory_path)
     os.chroot(directory_path)
     command = os.path.join("/", os.path.basename(command))
-    completed_process = subprocess.run([command, *args], capture_output=True)
+
+    completed_process = subprocess.run(
+        ["unshare", "--fork", "--pid", "--mount-proc", command, *args],
+        capture_output=True
+    )
+
     print(completed_process.stdout.decode(), end="")
     print(completed_process.stderr.decode(), end="", file=sys.stderr)
+
     exit(completed_process.returncode)
 
 if __name__ == "__main__":
