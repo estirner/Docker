9107bac54647c1480f3c09fe37cb664a03da2857
Sheroze <154746371+estirner@users.noreply.github.com>
2024-01-23 23:46:22 +0500

    pass sixth stage

--- app/main.py
+++ app/main.py
@@ -4,12 +4,45 @@ import subprocess
 import sys
 import tempfile
 import ctypes
+import requests
+import tarfile
+import json
+
+DOCKER_REGISTRY = "https://registry-1.docker.io"
+
+def authenticate(image):
+    response = requests.get(f"{DOCKER_REGISTRY}/v2/{image}/manifests/latest", headers={"Accept": "application/vnd.docker.distribution.manifest.v2+json"})
+    auth_url = response.headers["Www-Authenticate"].split(" ")[1].split(",")[0].split("=")[1].strip('"')
+    token_response = requests.get(auth_url)
+    token = token_response.json()["token"]
+    return token
+
+def fetch_manifest(image, token):
+    headers = {
+        "Accept": "application/vnd.docker.distribution.manifest.v2+json",
+        "Authorization": f"Bearer {token}"
+    }
+    response = requests.get(f"{DOCKER_REGISTRY}/v2/{image}/manifests/latest", headers=headers)
+    return response.json()
+
+def pull_and_extract_layers(image, manifest, token, directory_path):
+    headers = {"Authorization": f"Bearer {token}"}
+    for layer in manifest["layers"]:
+        response = requests.get(f"{DOCKER_REGISTRY}/v2/{image}/blobs/{layer['digest']}", headers=headers, stream=True)
+        file = tarfile.open(fileobj=response.raw, mode="r|*")
+        file.extractall(path=directory_path)
 
 def main():
+    image = "library/python"
     command = sys.argv[3]
     args = sys.argv[4:]
 
+    token = authenticate(image)
+    manifest = fetch_manifest(image, token)
+
     directory_path = tempfile.mkdtemp()
+    pull_and_extract_layers(image, manifest, token, directory_path)
+
     shutil.copy2(command, directory_path)
     os.chroot(directory_path)
     command = os.path.join("/", os.path.basename(command))
